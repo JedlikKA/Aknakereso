6e6c49bf95b93d82312e801e2626fdecfbaf5e62
Körmöndi.Alex.István <Krmndi.Alex.Istvn@C102-05>
2020-02-04 14:04:23 +0100

    Kész az aknakereső.

--- Aknakereso/Program.cs
+++ Aknakereso/Program.cs
@@ -8,17 +8,54 @@ namespace Aknakereso
 {
     class Program
     {
-        char[,] pálya = new char[10, 10];
         static void Main(string[] args)
         {
-            Feltöltés();
-            Console.WriteLine("Update");
+            char[,] pálya = new char[10, 10];
+            Feltölt(pálya);
+            Kirajzoló(pálya);
             Console.ReadKey();
         }
 
-        static void Feltöltés()
+
+        static void Kirajzoló(char[,] pálya)
         {
+
+
+            for (int sor = 0; sor < pálya.GetLength(0); sor++)
+            {
+                for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write($"{pálya[sor, oszlop]}|");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Feltölt(char[,] pálya)
+        {
+            int sor;
+            int oszlop;
+            for ( sor = 0; sor < pálya.GetLength(0); sor++)
+            {
+                for ( oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                {
+                    pálya[sor, oszlop] = '_';
+                }
+            }
+            Random gép = new Random();
             
+            for (int i = 0; i < 10; i++)
+            {
+                do
+                {
+                    sor = gép.Next(0, 10);
+                    oszlop = gép.Next(0, 10);
+                } while (pálya[sor, oszlop] == 'B');
+                pálya[sor, oszlop] = 'B';
+            }
+
         }
     }
 }
